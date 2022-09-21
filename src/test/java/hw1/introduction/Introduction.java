7ca2559110a68b17a961807de1e66ff0600c34b9
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-09-21 12:06:43 -0300

    add introduction test

---
+++ src/test/java/hw1/introduction/Introduction.java
@@ -0,0 +1,14 @@
+package hw1.introduction;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Introduction {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
