a87efe1ff4e0ed84eed7f19bfd9a5dc08b2f35e6
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-09-26 10:03:13 -0300

    add imports

--- src/test/java/hw1/introduction/Locators.java
+++ src/test/java/hw1/introduction/Locators.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.List;
 
